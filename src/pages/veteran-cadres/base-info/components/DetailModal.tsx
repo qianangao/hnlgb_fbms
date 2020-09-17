7feff9856ef4a96e51c89a4f1be9b831e0c06c84
段靖宇 <duanjingyu@xin.com>
2020-09-17 09:46:17 +0800

    feat: 新增老干部基本信息详情页

--- src/pages/veteran-cadres/base-info/components/DetailModal.tsx
+++ src/pages/veteran-cadres/base-info/components/DetailModal.tsx
@@ -1,18 +1,60 @@
 import React, { useEffect } from 'react';
 import { connect } from 'umi';
-import { Modal, Button } from 'antd';
+import { Modal, Descriptions, Spin, Button } from 'antd';
 
-const DetailModal = ({ dispatch, detailModalVisible, actionRef }) => {
-  const showModal = () => {
+const DetailModal = ({
+  dispatch,
+  detailModalVisible,
+  actionRef,
+  lgbDetailData,
+  lgbFamilyData,
+  lgbPartTimeData,
+  lgbHealthyData,
+  enums,
+  loading,
+}) => {
+  const showModal = id => {
     dispatch({
       type: 'vcBasicInfo/save',
       payload: {
         detailModalVisible: true,
       },
     });
+    if (id) {
+      dispatch({
+        type: 'vcBasicInfo/getLgbDetail',
+        payload: { id },
+      });
+      dispatch({
+        type: 'vcBasicInfo/getFamilyLgb',
+        payload: { id },
+      });
+      dispatch({
+        type: 'vcBasicInfo/getPartTimeLgb',
+        payload: { id },
+      });
+      dispatch({
+        type: 'vcBasicInfo/getHealthyLgb',
+        payload: { id },
+      });
+    }
   };
 
   useEffect(() => {
+    dispatch({
+      type: 'global/getEnums',
+      payload: {
+        names: [
+          'dictNation',
+          'dictRetirementLevel',
+          'dictRetirementType',
+          'dictSex',
+          'dictTreatmentNow',
+          'dictPoliticalStatus',
+        ],
+      },
+    });
+
     if (actionRef && typeof actionRef === 'function') {
       actionRef({ showModal });
     }
@@ -45,17 +87,152 @@ const DetailModal = ({ dispatch, detailModalVisible, actionRef }) => {
       destroyOnClose
       onCancel={hideModal}
       footer={[
-        <Button type="primary" onClick={hideModal}>
+        <Button key="ok" type="primary" onClick={hideModal}>
           确认
         </Button>,
       ]}
     >
-      detail
+      <Spin spinning={loading}>
+        <Descriptions title="基本信息" column={{ xxl: 4, xl: 3, lg: 2 }}>
+          <Descriptions.Item label="姓名">{lgbDetailData.realName}</Descriptions.Item>
+          <Descriptions.Item label="曾用名">{lgbDetailData.nameUsedBefore}</Descriptions.Item>
+          <Descriptions.Item label="性别">
+            {enums.dictSex && enums.dictSex[lgbDetailData.dictSex]}
+          </Descriptions.Item>
+          <Descriptions.Item label="工作单位">{lgbDetailData.spouseUnit}</Descriptions.Item>
+          <Descriptions.Item label="籍贯">{lgbDetailData.nativePlace}</Descriptions.Item>
+          <Descriptions.Item label="身份证号">{lgbDetailData.idCard}</Descriptions.Item>
+          <Descriptions.Item label="手机号码">{lgbDetailData.phonenumber}</Descriptions.Item>
+          <Descriptions.Item label="现管单位">{lgbDetailData.nowThePipeUnits}</Descriptions.Item>
+          <Descriptions.Item label="单位性质">
+            {enums.dictUnitNature && enums.dictUnitNature[lgbDetailData.dictUnitNature]}
+          </Descriptions.Item>
+          <Descriptions.Item label="民族">
+            {enums.dictUnitNature && enums.dictUnitNature[lgbDetailData.dictUnitNature]}
+          </Descriptions.Item>
+          <Descriptions.Item label="出生日期">{lgbDetailData.dateOfBirth}</Descriptions.Item>
+          <Descriptions.Item label="入党时间">{lgbDetailData.partyName}</Descriptions.Item>
+          <Descriptions.Item label="政治面貌">
+            {enums.dictPoliticalStatus &&
+              enums.dictPoliticalStatus[lgbDetailData.dictPoliticalStatus]}
+          </Descriptions.Item>
+          <Descriptions.Item label="生日">{lgbDetailData.birthday}</Descriptions.Item>
+          <Descriptions.Item label="离退休类型">
+            {enums.dictRetirementType && enums.dictRetirementType[lgbDetailData.dictRetirementType]}
+          </Descriptions.Item>
+          <Descriptions.Item label="文化程度">
+            {enums.dictDegree && enums.dictDegree[lgbDetailData.dictDegree]}
+          </Descriptions.Item>
+          <Descriptions.Item label="参加工作时间">{lgbDetailData.startWorkTime}</Descriptions.Item>
+          <Descriptions.Item label="离退休时间">{lgbDetailData.retirementDate}</Descriptions.Item>
+          <Descriptions.Item label="现享受待遇批准时间">
+            {lgbDetailData.treatmentApproveTime}
+          </Descriptions.Item>
+          <Descriptions.Item label="现享受待遇">
+            {enums.dictTreatmentNow && enums.dictTreatmentNow[lgbDetailData.dictTreatmentNow]}
+          </Descriptions.Item>
+          <Descriptions.Item label="职级">
+            {enums.dictRetirementLevel &&
+              enums.dictRetirementLevel[lgbDetailData.dictRetirementLevel]}
+          </Descriptions.Item>
+          <Descriptions.Item label="原工作单位及职务">
+            {lgbDetailData.originalUnitAndPosition}
+          </Descriptions.Item>
+          <Descriptions.Item label="原工作单位性质">
+            {enums.dictAllergenUnitNaturel &&
+              enums.dictAllergenUnitNaturel[lgbDetailData.dictAllergenUnitNaturel]}
+          </Descriptions.Item>
+          <Descriptions.Item label="层级">
+            {enums.hierarchy && enums.hierarchy[lgbDetailData.hierarchy]}
+          </Descriptions.Item>
+          <Descriptions.Item label="统计标志">{lgbDetailData.statisticSymbol}</Descriptions.Item>
+          <Descriptions.Item label="是否异地居住">
+            {lgbDetailData.isDifferentLive === 1 ? '是' : '否'}
+          </Descriptions.Item>
+          <Descriptions.Item label="待遇批准文号">
+            {lgbDetailData.treatmentApprovalNumber}
+          </Descriptions.Item>
+          <Descriptions.Item label="提高享受待遇情况">
+            {lgbDetailData.improveTreatment}
+          </Descriptions.Item>
+          <Descriptions.Item label="参加革命工作时期">
+            {enums.dictRevolutionPeriod &&
+              enums.dictRevolutionPeriod[lgbDetailData.dictRevolutionPeriod]}
+          </Descriptions.Item>
+          <Descriptions.Item label="健康装态">
+            {enums.dictHealth && enums.dictHealth[lgbDetailData.dictHealth]}
+          </Descriptions.Item>
+          <Descriptions.Item label="享受医疗待遇情况">
+            {enums.dictMedicalTreatment &&
+              enums.dictMedicalTreatment[lgbDetailData.dictMedicalTreatment]}
+          </Descriptions.Item>
+          <Descriptions.Item label="奖罚情况">{lgbDetailData.awardSituation}</Descriptions.Item>
+          <Descriptions.Item label="特殊贡献">
+            {lgbDetailData.specialContribution}
+          </Descriptions.Item>
+          <Descriptions.Item label="是否在世">
+            {lgbDetailData.isDead === 1 ? '是' : '否'}
+          </Descriptions.Item>
+          <Descriptions.Item label="离世时间">{lgbDetailData.dieDate}</Descriptions.Item>
+          <Descriptions.Item label="身份性质">{lgbDetailData.dictIdentity}</Descriptions.Item>
+        </Descriptions>
+        <Descriptions title="家庭信息" column={{ xxl: 4, xl: 3, lg: 2 }}>
+          <Descriptions.Item label="常住地址" span={{ xxl: 4, xl: 3, lg: 2 }}>
+            {`${lgbFamilyData.residentAddressList} ${lgbFamilyData.residentAddressDiy}`}
+          </Descriptions.Item>
+          <Descriptions.Item label="家庭住址" span={{ xxl: 4, xl: 3, lg: 2 }}>
+            {`${lgbFamilyData.homeNameList} ${lgbFamilyData.homeAddressDiy}`}
+          </Descriptions.Item>
+
+          <Descriptions.Item label="婚姻状况">
+            {enums.dictMarriage && enums.dictMarriage[lgbFamilyData.dictMarriage]}
+          </Descriptions.Item>
+          <Descriptions.Item label="居住状态">
+            {enums.dictLiveStatu && enums.dictLiveStatu[lgbFamilyData.dictLiveStatu]}
+          </Descriptions.Item>
+          <Descriptions.Item label="购房情况">{lgbFamilyData.purchaseSituation}</Descriptions.Item>
+          <Descriptions.Item label="住房建筑面积">{lgbFamilyData.structureArea}</Descriptions.Item>
+          <Descriptions.Item label="子女数">{lgbFamilyData.childrenNum}</Descriptions.Item>
+          <Descriptions.Item label="无劳动能力子女数">
+            {lgbFamilyData.noworkChildrenNum}
+          </Descriptions.Item>
+          <Descriptions.Item label="赡养人数">{lgbFamilyData.supportNum}</Descriptions.Item>
+          <Descriptions.Item label="抚养人数">{lgbFamilyData.dependencyNum}</Descriptions.Item>
+          <Descriptions.Item label="住宅电话">{lgbFamilyData.telephone}</Descriptions.Item>
+          <Descriptions.Item label="固定电话1">{lgbFamilyData.telephone1}</Descriptions.Item>
+          <Descriptions.Item label="固定电话2">{lgbFamilyData.telephone2}</Descriptions.Item>
+          <Descriptions.Item label="集团号码">{lgbFamilyData.groupNumber}</Descriptions.Item>
+          <Descriptions.Item label="邮编">{lgbFamilyData.postCode}</Descriptions.Item>
+        </Descriptions>
+
+        <Descriptions title="兼职信息" column={{ xxl: 4, xl: 3, lg: 2 }}>
+          <Descriptions.Item label="社会团体">{lgbPartTimeData.socialGroups}</Descriptions.Item>
+          <Descriptions.Item label="职务">{lgbPartTimeData.post}</Descriptions.Item>
+          <Descriptions.Item label="兼职常驻地">
+            {lgbPartTimeData.placeOfResidenceName}
+          </Descriptions.Item>
+          <Descriptions.Item label="详细地址">{lgbPartTimeData.detailedAddress}</Descriptions.Item>
+        </Descriptions>
+
+        <Descriptions title="健康档案" column={{ xxl: 4, xl: 3, lg: 2 }}>
+          <Descriptions.Item label="健康状态">{lgbHealthyData.dictHealth}</Descriptions.Item>
+          <Descriptions.Item label="疾病情况">{lgbHealthyData.disease}</Descriptions.Item>
+          <Descriptions.Item label="合同医院">{lgbHealthyData.assignedHospital}</Descriptions.Item>
+          <Descriptions.Item label="评残情况">{lgbHealthyData.disability}</Descriptions.Item>
+          <Descriptions.Item label="就近医院">{lgbHealthyData.nearHospital}</Descriptions.Item>
+          <Descriptions.Item label="医疗照顾">{lgbHealthyData.caregivers}</Descriptions.Item>
+        </Descriptions>
+      </Spin>
     </Modal>
   );
 };
 
-export default connect(({ vcBasicInfo, loading }) => ({
+export default connect(({ vcBasicInfo, loading, global }) => ({
+  lgbDetailData: vcBasicInfo.lgbDetailData,
+  lgbFamilyData: vcBasicInfo.lgbFamilyData,
+  lgbPartTimeData: vcBasicInfo.lgbPartTimeData,
+  lgbHealthyData: vcBasicInfo.lgbHealthyData,
   detailModalVisible: vcBasicInfo.detailModalVisible,
   loading: loading.models.vcBasicInfo,
+  enums: global.enums,
 }))(DetailModal);
