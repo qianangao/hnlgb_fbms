78f267ea1ca529a7f12f84a2e63f0cb9387b43c7
jiel <a644089172XL*>
2020-09-18 10:15:44 +0800

    去掉 每日播报增删改

--- src/pages/information-release/daily-broadcast/components/Table.tsx
+++ src/pages/information-release/daily-broadcast/components/Table.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Button, Modal } from 'antd';
 import ProTable from '@ant-design/pro-table';
 import { connect } from 'umi';
 
-const Table = ({ openAddModal, openModifyModal, dailyBroadcast, dispatch, opendetailModal }) => {
-  const { tableRef, publishStatus } = dailyBroadcast;
+const Table = ({ dailyBroadcast, dispatch, opendetailModal }) => {
+  const { tableRef } = dailyBroadcast;
   const columns = [
     {
       title: '序号',
@@ -26,10 +25,10 @@ const Table = ({ openAddModal, openModifyModal, dailyBroadcast, dispatch, opende
       hideInSearch: true,
     },
     {
-      title: publishStatus === 0 ? '保存时间' : '发布时间',
+      title: '保存时间',
       valueType: 'date',
       align: 'center',
-      dataIndex: publishStatus === 0 ? 'createTime' : 'releaseTime',
+      dataIndex: 'createTime',
       hideInSearch: true,
     },
     {
@@ -40,25 +39,14 @@ const Table = ({ openAddModal, openModifyModal, dailyBroadcast, dispatch, opende
       width: 180,
       fixed: 'right',
       render: (dom, employeeData) => [
-        publishStatus === 0 ? (
-          <a
-            key={`${employeeData.id}up`}
-            onClick={() => {
-              openModifyModal(employeeData);
-            }}
-          >
-            编辑
-          </a>
-        ) : (
-          <a
-            key={`${employeeData.id}up`}
-            onClick={() => {
-              opendetailModal(employeeData);
-            }}
-          >
-            详情
-          </a>
-        ),
+        <a
+          key={`${employeeData.id}up`}
+          onClick={() => {
+            opendetailModal(employeeData);
+          }}
+        >
+          详情
+        </a>,
       ],
     },
   ];
@@ -72,15 +60,6 @@ const Table = ({ openAddModal, openModifyModal, dailyBroadcast, dispatch, opende
         resolve,
       });
     });
-  // 删除
-  const deleteReturnworkPerson = ids => {
-    dispatch({
-      type: 'dailyBroadcast/deleteDailyBroadcast',
-      payload: {
-        ids,
-      },
-    });
-  };
 
   return (
     <ProTable
@@ -90,28 +69,6 @@ const Table = ({ openAddModal, openModifyModal, dailyBroadcast, dispatch, opende
       rowSelection={[]}
       scroll={{ x: 'max-content' }}
       request={async params => getEmployeeList(params)}
-      toolBarRender={(_, { selectedRowKeys }) => [
-        publishStatus === 0 ? (
-          <Button type="primary" onClick={() => openAddModal()}>
-            新增
-          </Button>
-        ) : null,
-        selectedRowKeys && selectedRowKeys.length && (
-          <Button
-            onClick={() => {
-              Modal.confirm({
-                title: '确认删除每日播报？',
-                content: '一旦确定将无法恢复',
-                onOk: () => {
-                  deleteReturnworkPerson(selectedRowKeys);
-                },
-              });
-            }}
-          >
-            批量删除
-          </Button>
-        ),
-      ]}
       columns={columns}
     />
   );
--- src/pages/information-release/daily-broadcast/service.ts
+++ src/pages/information-release/daily-broadcast/service.ts
@@ -12,28 +12,6 @@ export async function dailyBroadcastList(params) {
 }
 
 /**
- * 删除-每日播报
- * @param {*} params
- */
-export async function deleteDailyBroadcast(params) {
-  return request(`/dailyBroadcast`, {
-    method: 'DELETE',
-    data: params,
-  });
-}
-
-/**
- * 新增-每日播报
- * @param {*} params
- */
-export async function addDailyBroadcast(params) {
-  return request(`/dailyBroadcast`, {
-    method: 'POST',
-    data: params,
-  });
-}
-
-/**
  * 每日播报-详情
  * @param {*} params
  */
@@ -41,16 +19,5 @@ export async function detailDailyBroadcast(params) {
   return request(`/dailyBroadcast/${params.id}`, {
     method: 'GET',
     params,
-  });
-}
-
-/**
- * 编辑-每日播报
- * @param {*} params
- */
-export async function updateDailyBroadcast(params) {
-  return request(`/dailyBroadcast/${params.id}`, {
-    method: 'PUT',
-    data: params,
   });
 }
