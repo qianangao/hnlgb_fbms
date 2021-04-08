92b27c672b4efe37fcc9c57fb6010dcd60600146
jiel <a644089172XL*>
2021-04-08 18:21:34 +0800

    修改：支部信息-编辑成员-新增成员：身份证精确匹配全系统相应的老同志

--- src/models/global/lgb.ts
+++ src/models/global/lgb.ts
@@ -1,4 +1,4 @@
-import { getLgbList } from '@/services/global/lgb';
+import { getLgbList, getSystemLgbList } from '@/services/global/lgb';
 
 const Model = {
   namespace: 'globalLgb',
@@ -17,6 +17,39 @@ const Model = {
       };
 
       const response = yield call(getLgbList, params);
+
+      if (!response.error) {
+        const { items, currentPage, totalNum } = response;
+
+        const result = {
+          data: items,
+          page: currentPage,
+          pageSize: payload.pageSize,
+          success: true,
+          total: totalNum,
+        };
+
+        resolve && resolve(result);
+
+        yield put({
+          type: 'save',
+          payload: {
+            lgbListData: result,
+          },
+        });
+      }
+    },
+    *getSystemLgbList({ payload, resolve }, { call, put, select }) {
+      const { organizationId } = yield select(state => state.user.userInfo);
+
+      const params = {
+        ...payload,
+        orgIdForDataSelect: payload.orgIdForDataSelect || organizationId,
+        currentPage: payload.current,
+        pageSize: payload.pageSize,
+      };
+
+      const response = yield call(getSystemLgbList, params);
 
       if (!response.error) {
         const { items, currentPage, totalNum } = response;
